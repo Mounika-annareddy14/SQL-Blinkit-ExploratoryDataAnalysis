b5ee7817accc794997422fc0b10b3101371727cf
Mounika Annareddy <160157282+Mounika-annareddy14@users.noreply.github.com>
2026-02-07 19:05:59 +0530

    Update database_exploration.sql

--- Scripts/database_exploration.sql
+++ Scripts/database_exploration.sql
@@ -1,1 +1,42 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
 
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+use Blinkit_Dwh;
+
+
+--Explore All Objects in the Database
+SELECT* FROM INFORMATION_SCHEMA.Tables;
+
+-- Retrieve a list of all tables in the database
+SELECT 
+    TABLE_CATALOG, 
+    TABLE_SCHEMA, 
+    TABLE_NAME, 
+    TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
+
+
+--EXPLORE All columns in the Database
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE
+TABLE_NAME = 'dim_customers'
+  
+-- Retrieve all columns for a specific table (dim_customers)
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
